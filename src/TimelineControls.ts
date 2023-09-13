4c611f7f328f054187b223b6a24b288994be61a9
André "MG" Wisén <andre.wisen@gmail.com>
2023-09-13 12:22:17 +0200

    refactor `example`

---
+++ src/TimelineControls.ts
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+export default class TimelineControls extends THREE.EventDispatcher {
+    constructor() {
+        super();
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { default as TimelineObject3D } from './TimelineObject3D';
+export { default as ComplexTimelineObject3D } from './ComplexTimelineObject3D';
+
+export { default as TimelineControls } from './TimelineControls';
+
 export * from './extensions';
 export * from './types';
